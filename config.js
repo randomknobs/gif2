63c27b815002341824859a2938b716555deaebbb
randomknobs <75247153+randomknobs@users.noreply.github.com>
2023-08-26 19:16:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var giphyTVDescription = "";
 var giphyTVSearch = "";
 
 // Delay between GIFs (5000=5seconds)
-var giphyTVDelay = 6500;
+var giphyTVDelay = 6000;
 
 // MAGIC! (don't touch, it hurts)
 document.getElementById("giphyTVUsername").innerHTML = giphyTVUsername;
