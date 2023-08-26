68eec11d7e9d7e600a94f248373ab348ee747c20
randomknobs <75247153+randomknobs@users.noreply.github.com>
2023-08-26 18:44:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,16 @@
 var giphyAPIKey = "0HaE831ZSEgJOV6ELIn13XPVEZRiif5S";
 
 // The title of your own GiphyTV :)
-var giphyTVUsername = "Cats are everywhere";
+var giphyTVUsername = "";
 
 // The description where you can put some HTML inside!
-var giphyTVDescription = "Nap all day friends. Decide to do nothing with the owners today. Go meow loudly just to annoy owners. Eat owner's food. Jump on humans while they sleep, do it all night long. Go, be like me.<br /><br />Be a stupid cat.";
+var giphyTVDescription = "";
 
 // Your category of Giphy GIFs, only one word please.
-var giphyTVSearch = "kitten";
+var giphyTVSearch = "";
 
 // Delay between GIFs (5000=5seconds)
-var giphyTVDelay = 5000;
+var giphyTVDelay = 10000;
 
 // MAGIC! (don't touch, it hurts)
 document.getElementById("giphyTVUsername").innerHTML = giphyTVUsername;
