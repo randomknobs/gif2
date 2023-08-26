1ed8e019f7269e299f3d384fbd11cd68e44b6018
randomknobs <75247153+randomknobs@users.noreply.github.com>
2023-08-26 19:18:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var giphyTVUsername = "";
 var giphyTVDescription = "";
 
 // Your category of Giphy GIFs, only one word please.
-var giphyTVSearch = "ass+strings+sunrise";
+var giphyTVSearch = "war";
 
 // Delay between GIFs (5000=5seconds)
 var giphyTVDelay = 6000;
